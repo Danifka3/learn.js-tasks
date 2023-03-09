a9cf8b441dfd1f37c7ec520629114813a6a46c13
Danifka3 <danilagromyko24@gmail.com>
2023-03-09 21:56:30 +0300

    sum-mas-elem with ''

--- sum-mas-elem/app.js
+++ sum-mas-elem/app.js
@@ -1,11 +1,13 @@
 function sumInput() {
     let num = prompt('Введите число:', '0');
     let numMas = [];
-    let sum = 0;
-    while (num !== null && !isNaN(+num)) {
+
+    while (num !== '' && num !== null && !isFinite(+num)) {
         numMas.push(+num);
         num = prompt('Введите число:', '0');
     }
+
+    let sum = 0;
     for (let elem of numMas) {
         sum += elem;
     }
